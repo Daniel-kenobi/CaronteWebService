2bfeb2124801b479bc519e62673a38f2b2c6ec08
daniel-kenobi <daniel.companyservicostecnicos@gmail.com>
2023-04-27 13:45:58 -0300

    More updates

--- Application/Mediators/Login/LoginQueryHandler.cs
+++ Application/Mediators/Login/LoginQueryHandler.cs
@@ -1,41 +1,64 @@
-﻿using Barsa.CommomResponses;
+﻿using AutoMapper;
+using Barsa.CommomResponses;
+using Barsa.Models.CreateClientUser;
+using Barsa.Models.JWTAppSettingsModel;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Tartaro.Configurations;
 using Tartaro.Data;
+using Tartaro.Data.Entities;
 
 namespace Tartaro.Application.Mediators.Login
 {
-    public class LoginQueryHandler : IRequestHandler<LoginQuery, CommomMediatorResponse<string>>
+    public class LoginQueryHandler : HandleJWT, IRequestHandler<LoginQuery, CommomMediatorResponse<string>>
     {
         private readonly ApplicationDbContext _dbContext;
-        public LoginQueryHandler(ApplicationDbContext dbContext)
+        private readonly IMapper _mapper;
+        public LoginQueryHandler(ApplicationDbContext dbContext, IMapper mapper, IOptions<JWTAppSettings> jwtModel) : base(jwtModel)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
-        public Task<CommomMediatorResponse<string>> Handle(LoginQuery request, CancellationToken cancellationToken)
+        public async Task<CommomMediatorResponse<string>> Handle(LoginQuery request, CancellationToken cancellationToken)
         {
-            var response = new CommomMediatorResponse<string>("");
+            var response = new CommomMediatorResponse<string>();
 
             try
             {
-
+                response.ResponseObject = await HandleUser(request.Client);
             }
             catch (Exception ex)
             {
 
             }
 
-            return Task.FromResult(response);
+            return response;
         }
 
-        private bool ValidateUser(LoginQuery request)
+        private async Task<string> HandleUser(ClientUserModel user)
         {
-            var validated = false;
+            if (!await UserExists(user))
+                await CreateUser(user);
 
-            if (request.Client is null)
-                throw new ArgumentNullException("Credenciais nulas.");
+            return GenerateToken(user);
+        }
 
-            return validated;
+        private async Task<bool> UserExists(ClientUserModel user)
+        {
+            return (await _dbContext.Users.FirstOrDefaultAsync(x => x.Username == user.Username && x.WindowsVersion == user.OSVersion) is not null);
+        }
+
+        private async Task CreateUser(ClientUserModel user)
+        {
+            var userEntity = MapUserModelToEntity(user);
+            await _dbContext.Users.AddAsync(userEntity);
+        }
+
+        private User MapUserModelToEntity(ClientUserModel user)
+        {
+            return _mapper.Map<User>(user);
         }
     }
 }
---
+++ Configurations/HandleJWT.cs
@@ -0,0 +1,46 @@
+﻿using Barsa.Models.CreateClientUser;
+using Barsa.Models.JWTAppSettingsModel;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tartaro.Data.Entities;
+
+namespace Tartaro.Configurations
+{
+    public abstract class HandleJWT
+    {
+        private readonly IOptions<JWTAppSettings> _jwtModel;
+        public HandleJWT(IOptions<JWTAppSettings> jwtModel)
+        {
+            _jwtModel = jwtModel;
+        }
+
+        public string GenerateToken(ClientUserModel user)
+        {
+            var issuer = _jwtModel.Value.Issuer;
+            var audience = _jwtModel.Value.Audience;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtModel.Value.Key));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = GenerateUserClaims(user);
+            var token = new JwtSecurityToken(issuer: issuer, audience: audience, expires: DateTime.Now.AddMinutes(120), signingCredentials: credentials, claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        }
+
+        private List<Claim> GenerateUserClaims(ClientUserModel user)
+        {
+            return new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Version, user.OSVersion)
+            };
+        }
+    }
+}
