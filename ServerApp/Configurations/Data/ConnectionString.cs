2d3c7a54fe1abf43b17fdbc79e5abe785cd06aac
daniel-kenobi <daniel.companyservicostecnicos@gmail.com>
2023-06-14 21:56:44 -0300

    More updates

--- ServerApp/Configurations/Data/ConnectionString.cs
+++ ServerApp/Configurations/Data/ConnectionString.cs
@@ -1,10 +1,12 @@
-﻿namespace Tartaro.Configurations.Data
+﻿using Caronte.Infra.Configs;
+
+namespace Tartaro.Configurations.Data
 {
     public static class ConnectionString
     {
         public static IServiceCollection BindConnectionStringToModel(WebApplicationBuilder builder)
         {
-            builder.Services.Configure<Barsa.Models.Database>(builder.Configuration.GetSection("ConnectionString"));
+            builder.Services.Configure<DatabaseConnectionStringModel>(builder.Configuration.GetSection("ConnectionString"));
             return builder.Services;
         }
     }
